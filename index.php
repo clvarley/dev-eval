c3cc8f2612d4e9e61fb288a5b89b5e6f3b222778
Ryan Andrews <ryan.andrews@network-n.com>
2024-09-18 11:38:50 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Developer Test</title>
+
+    <link rel="stylesheet" href="./dist/css/style.min.css">
+
+    <style>
+        <?php echo file_get_contents('./dist/css/critical.min.css'); ?>
+    </style>
+</head>
+<body>
+    <div class="container">
+        <img src="./dist/images/logo.svg" alt="Logo">
+    
+        <h1>Developer Test</h1>
+    
+        <img class="hero" src="./dist/images/image-1.jpg" alt="Gaming Accessories">
+
+        <h2>Lorem ipsum dolor</h2>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut voluptas est, incidunt sapiente nihil modi possimus accusamus in reiciendis! Nam quisquam tenetur, hic minus perspiciatis omnis in unde ipsum ex veniam impedit, cum obcaecati exercitationem a sapiente consectetur nihil?</p>
+            
+        <p>Fuga reprehenderit dicta soluta sapiente consectetur a accusamus? Sint, eveniet perferendis aspernatur quidem fuga exercitationem laboriosam modi doloremque aut eos praesentium obcaecati excepturi accusantium at culpa ex deleniti qui veniam provident recusandae ducimus esse natus dicta. Maiores voluptate quibusdam, ratione debitis sunt repellat nisi dicta a dolore illum, velit inventore rem et aspernatur quis aliquam. Voluptate cumque ipsa illum ducimus, voluptatibus laborum nihil rerum tempore consequatur nulla cupiditate mollitia, excepturi est odit aperiam illo. Consequuntur qui quo architecto dolore sequi magni animi. Asperiores doloremque iusto ea porro totam, possimus atque esse exercitationem nulla unde deleniti sapiente veritatis excepturi sequi odit officiis dicta blanditiis voluptatum? Distinctio cupiditate adipisci nemo maxime temporibus maiores at officia exercitationem quia inventore!</p>
+        
+        <p>Quaerat molestiae ut cum, praesentium esse totam maxime eos optio rem voluptatum eaque quas aspernatur porro amet officia facilis distinctio expedita magni maiores quos nam quisquam dolorum unde voluptatem! Iusto hic, officiis, optio, quo nemo in eaque laudantium beatae sequi laborum ab quasi incidunt fugiat?</p>
+
+        <h2>Officia facilis distinctio</h2>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut voluptas est, incidunt sapiente nihil modi possimus accusamus in reiciendis! Nam quisquam tenetur, hic minus perspiciatis omnis in unde ipsum ex veniam impedit, cum obcaecati exercitationem a sapiente consectetur nihil?</p>
+            
+        <p>Fuga reprehenderit dicta soluta sapiente consectetur a accusamus? Sint, eveniet perferendis aspernatur quidem fuga exercitationem laboriosam modi doloremque aut eos praesentium obcaecati excepturi accusantium at culpa ex deleniti qui veniam provident recusandae ducimus esse natus dicta. Maiores voluptate quibusdam, ratione debitis sunt repellat nisi dicta a dolore illum, velit inventore rem et aspernatur quis aliquam. Voluptate cumque ipsa illum ducimus, voluptatibus laborum nihil rerum tempore consequatur nulla cupiditate mollitia, excepturi est odit aperiam illo. Consequuntur qui quo architecto dolore sequi magni animi. Asperiores doloremque iusto ea porro totam, possimus atque esse exercitationem nulla unde deleniti sapiente veritatis excepturi sequi odit officiis dicta blanditiis voluptatum? Distinctio cupiditate adipisci nemo maxime temporibus maiores at officia exercitationem quia inventore!</p>
+        
+        <p>Quaerat molestiae ut cum, praesentium esse totam maxime eos optio rem voluptatum eaque quas aspernatur porro amet officia facilis distinctio expedita magni maiores quos nam quisquam dolorum unde voluptatem! Iusto hic, officiis, optio, quo nemo in eaque laudantium beatae sequi laborum ab quasi incidunt fugiat?</p>
+
+        <div class="images">
+            <img src="./dist/images/image-2.jpg" alt="Space Invader">
+            <img src="./dist/images/image-3.jpg" alt="Gaming PC">
+        </div>
+    </div>
+</body>
+</html>
