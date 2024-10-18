f23b1cbe6fbc44ace6968e4662da14312538c54a
Colin Polonowski <colinpolonowski@Colins-MacBook-Pro-2.local>
2024-10-18 12:35:02 +0100

    Debug

--- steamapi.php
+++ steamapi.php
@@ -23,7 +23,7 @@
     $steamData = file_get_contents('https://api.steampowered.com/ISteamApps/GetAppList/v2/');
     $steamData = json_decode($steamData, true);
     foreach ($steamData['applist']['apps'] as $app) {
-        echo $app['appid'] . ' - ' . htmlentities($app['name']) . '<br>'.PHP_EOL;
+        echo $app['appid'] . ' - ' . $app['name'] . '<br>'.PHP_EOL;
     }
 
     ?>
